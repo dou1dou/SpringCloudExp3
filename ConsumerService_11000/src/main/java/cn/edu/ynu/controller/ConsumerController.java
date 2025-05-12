973dda989218b4ea1e4bea31a7dabf56d974c8dc
doudou <14307850+dou-dou_04@user.noreply.gitee.com>
2025-05-12 17:07:10 +0800

    实验三提交

--- ConsumerService_11000/src/main/java/cn/edu/ynu/controller/ConsumerController.java
+++ ConsumerService_11000/src/main/java/cn/edu/ynu/controller/ConsumerController.java
@@ -1,6 +1,7 @@
 package cn.edu.ynu.controller;
 
 import cn.edu.ynu.feign.ServiceProviderService;
+import io.github.resilience4j.bulkhead.annotation.Bulkhead;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import jakarta.annotation.Resource;
@@ -18,6 +19,7 @@ public class ConsumerController {
     @GetMapping("/callFeign/backendA")
     @CircuitBreaker(name = "backendA", fallbackMethod = "helloGetFeignDown")
     @RateLimiter(name = "rate1", fallbackMethod = "helloGetFeignDown")
+    @Bulkhead(name = "bulkheadA", fallbackMethod = "helloGetFeignDown")
     public String helloGetFeignA() {
         return serviceInstance.helloGet();
     }
@@ -25,6 +27,7 @@ public class ConsumerController {
     @GetMapping("/callFeign/backendB")
     @CircuitBreaker(name = "backendB", fallbackMethod = "helloGetFeignDown")
     @RateLimiter(name = "rate1", fallbackMethod = "helloGetFeignDown")
+    @Bulkhead(name = "bulkheadB", fallbackMethod = "helloGetFeignDown")
     public String helloGetFeignB() {
         return serviceInstance.helloGet();
     }
